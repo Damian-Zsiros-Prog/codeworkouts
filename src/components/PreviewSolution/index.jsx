8d216ae6e024d3943e999bc1602abadf85e55253
Damian Zsiros <damjeronzalez23@gmail.com>
2023-03-14 01:56:59 -0500

    add code editor at preview

--- src/components/PreviewSolution/index.jsx
+++ src/components/PreviewSolution/index.jsx
@@ -1,12 +1,31 @@
-import { useState } from 'react'
+import CodeEditor from '@uiw/react-textarea-code-editor'
 import { useExerciseStore } from '../../stores/Exercise'
 
 const PreviewSolution = () => {
   const solution = useExerciseStore((state) => state.solution)
+  const language = useExerciseStore((state) => state.language)
+  const generatingSolution = useExerciseStore(
+    (state) => state.generatingSolution
+  )
   return (
-    <>
-      <p className='bg-gray-500 max-w-md'>{solution}</p>
-    </>
+    <CodeEditor
+      className='w-lg min-h-lg max-h-xl'
+      value={solution}
+      language={language}
+      minHeight={200}
+      placeholder={
+        generatingSolution
+          ? 'Generando solucion...'
+          : 'Aqui estara tu solucion.'
+      }
+      padding={15}
+      style={{
+        fontSize: 12,
+        backgroundColor: '#f5f5f5',
+        fontFamily:
+          'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace'
+      }}
+    />
   )
 }
 
