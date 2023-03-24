ae55f53a03eff1ff59b7be03468b9c8dadf76bfe
Damian <damianzsiros@unisinu.edu.co>
2023-03-24 14:21:57 -0500

    update class words for className

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer class='relative'>
+    <footer className='relative'>
       <section className='fixed bottom-0 w-full bg-blue-600 shadow dark:bg-blue-800'>
         <div className='w-full text-center mx-auto container md:p-6 p-4 md:flex md:items-center md:justify-between'>
           <span className='text-sm text-white sm:text-center '>
